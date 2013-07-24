6a86c98cc7a0a65e352e0e9d01a46263faadebbb
Trude <trude.martinsen@gmail.com>
2013-07-24 12:37:08 +0200

    Lagt til heroku procfile

--- src/main/java/no/tm/bryllupsliste/BryllupServlet.java
+++ src/main/java/no/tm/bryllupsliste/BryllupServlet.java
@@ -8,15 +8,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 public class BryllupServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-	resp.setContentType("text/html");
-	PrintWriter writer = resp.getWriter();
-	
-	writer.append("<html><body>").append("<h1>Hello from servlet</h1>").append("</body></html>");
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+
+		writer.append("<html><body>").append("<h1>Hello from servlet</h1>")
+				.append("</body></html>");
+	}
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(
+				ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new BryllupServlet()), "/*");
+		server.start();
+		server.join();
 	}
 
 }
