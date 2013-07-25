60659deec656010e0a9752b6eebd24a5bec2e27a
Trude <trude.martinsen@gmail.com>
2013-07-25 10:45:18 +0200

    Lagt til angular med kjørende test

---
+++ src/test/js/testSpec.js
@@ -0,0 +1,12 @@
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+
+describe("Noe annet", function() {
+	  it("contains spec with an bad expectation", function() {
+	    expect(true).toBe(true);
+	  });
+	});
